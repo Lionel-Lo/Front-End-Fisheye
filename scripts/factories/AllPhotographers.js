4ad20108c2534821c3bc8114c514d1896c6d3b2f
Lionel_lo <longlionel@aol.com>
2022-04-27 17:04:29 +0200

    my first commit

--- scripts/factories/AllPhotographers.js
+++ scripts/factories/AllPhotographers.js
@@ -2,29 +2,48 @@ export default class AllPhotographers{
     constructor(){
         this.DataPhotographers = [];
         this.getData();
+        //this.displayData();
     }
-
     getData(){
         fetch("/data/photographers.json")
             .then(res => {
                 if(res.ok){
-                    res.json().then((response)=>
-                    console.log(response.photographers),
-                    )
-                    this.DataPhotographers.push(response.photographers)
+                    return res.json();
                 }
-                console.log(this.DataPhotographers)
             })
-            // .catch(function(err){
-            //     console.error("bad")
-            // })
+            .then(data =>{
+                for (let i = 0; i < data["photographers"].length; i++) {
+                    this.DataPhotographers  = (data.photographers[i])
+                        console.log(data.photographers[i])
+                }  
+            })
+            .catch(function(err){
+                console.error("bad")
+            })
         //fetch = recup data allphotographers => stock dans tableau.
         //fonction assynchrone
         //verifi donées
-        console.log('toto');
     }
-
     displayData() {
-        // afficher data     
+       
+
+        const photographersSection = document.querySelector(".photographer_section");
+        console.log(photographersSection)
+
+
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerFactory(photographers);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
+        });
+
+        async function init() {
+            // Récupère les datas des photographes
+            const { photographers } = await getData();
+            displayData();
+        };
+    
+    init();  
+
     }
 }
